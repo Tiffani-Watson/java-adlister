0d63c075e9ddc1d93517a56297eef3299498a39e
Tiffani <tiffani.marie.watson@gmail.com>
2023-07-26 11:27:36 -0500

    updated files

--- src/main/java/com/codeup/adlister/controllers/LogoutServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LogoutServlet.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,5 +14,21 @@ public class LogoutServlet extends HttpServlet {
         request.getSession().removeAttribute("user");
         request.getSession().invalidate();
         response.sendRedirect("/login");
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        String username = null;
+        if (DaoFactory.getUsersDao().findByUsername(username) != null) {
+            try {
+                response.sendRedirect(request.getContextPath() + "/register?error=username");
+                return;
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Received form data:");
     }
+
+
 }
